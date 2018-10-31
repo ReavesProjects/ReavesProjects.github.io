593dee69ce87052778dd76664266b475f915cad9
Sharon Reaves <sharon@reavesgallery.com>
2018-10-30 20:38:07 -0400

    first commit

---
+++ finnis-project.js
@@ -0,0 +1,15 @@
+$("#gallery").on("click, mouseleave", function(){
+$("#gallery").fadeIn(2000)
+})
+      
+$(".link").click(function(e){
+e.preventDefault();
+$(".popup").fadeIn(300,function(){$(this).focus();});
+});
+
+$('.close').click(function() {
+   $(".popup").fadeOut(300);
+});
+$(".popup").on('blur',function(){
+    $(this).fadeOut(300);
+});
