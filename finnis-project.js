e5f9bc5775d829f530a5ad2f2a848ba689630930
Sharon Reaves <sharon@reavesgallery.com>
2018-11-04 09:58:26 -0500

    sunday am commit

--- finnis-project.js
+++ finnis-project.js
@@ -1,15 +1,43 @@
-$("#gallery").on("click, mouseleave", function(){
-$("#gallery").fadeIn(2000)
-})
-      
-$(".link").click(function(e){
-e.preventDefault();
-$(".popup").fadeIn(300,function(){$(this).focus();});
+$(document).ready(function() {
+
+    var delay = 300; // milliseconds
+    var cookie_expire = 0; // days
+
+    var cookie = localStorage.getItem("list-builder");
+    if(cookie == undefined || cookie == null) {
+        cookie = 0;
+    }
+
+    if(((new Date()).getTime() - cookie) / (1000 * 60 * 60 * 24) > cookie_expire) {
+        $("#list-builder").delay(delay).fadeIn("fast", () => {
+            $("#popup-box").fadeIn("fast", () => {});
+        });
+
+        $("button[name=subscribe]").click(() => {
+            $.ajax({
+                type: "POST",
+                url: $("#popup-form").attr("action"),
+                data: $("#popup-form").serialize(),
+                success: (data) => {
+                    $("#popup-box-content").html("<p style='text-align: center'>Thank you for subscribing to Finni's newsletter!</p>");
+                }
+            });
+        });
+
+        $("#popup-close").click(() => {
+            $("#list-builder, #popup-box").hide();
+            localStorage.setItem("list-builder", (new Date()).getTime());
+        });
+    }
+
 });
 
-$('.close').click(function() {
-   $(".popup").fadeOut(300);
-});
-$(".popup").on('blur',function(){
-    $(this).fadeOut(300);
-});
+			this.onclick = function(e){
+				if(e.target.getAttribute("src")){
+					$("#box").fadeToggle()
+					var address = e.target.getAttribute("src")
+					$("#pic2").attr("src", address)
+				}
+			}
+
+			
